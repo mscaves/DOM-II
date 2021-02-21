7f73dd198d6caeca790c2a1e55c4211092c34700
Michael Caves <michael.scott.caves@gmail.com>
2021-02-20 20:27:00 -0500

    Added Event Listeners

--- js/index.js
+++ js/index.js
@@ -1,1 +1,32 @@
 // Your code goes here
+let welcomeText = document.querySelector('.intro h2')
+welcomeText.addEventListener("click", () => {
+    welcomeText.textContent = 'Goodbye from the fun bus!';
+})
+
+let navSelector = document.querySelector('.container.nav-container');
+navSelector.addEventListener("mouseover", () => {
+    navSelector.style.color = 'pink';
+})
+
+let subTitle = document.querySelector('.text-content h2');
+window.addEventListener("keydown", () => {
+    subTitle.textContent = "I SAW THAT.";
+})
+
+let destinationPanel = document.querySelector(".destination");
+destinationPanel.addEventListener("dblclick", () => {
+    destinationPanel.style.color = "purple";
+})
+
+let header = document.querySelector('body header');
+header.addEventListener("dblclick", () => {
+    header.style.backgroundColor = 'purple';
+})
+
+let headerNav = document.querySelector('body header .nav');
+headerNav.addEventListener('dblclick', () => {
+    headerNav.stopPropogation();
+    headerNav.style.transform = "Scale(1.2)";
+    headerNav.style.transition = 'all 0.3s';
+})
